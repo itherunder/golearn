6756a003447ed244d9e0594afb3f7b1ec1747e86
itherunder <liaozhou98@qq.com>
2022-04-05 23:41:42 +0800

    learn query urls by goroutine

--- fetch_/fetch_test.go
+++ fetch_/fetch_test.go
@@ -5,24 +5,51 @@ import (
 	"net/http"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/yezihack/colorlog"
 )
 
-func TestFetch(t *testing.T) {
-	urls := []string{"http://www.baidu.com", "https://github.com/itherunder"}
-	for _, url := range urls {
-		resp, err := http.Get(url)
-		if err != nil {
-			colorlog.Error("fetch %s failed: %v", url, err)
-			os.Exit(1)
-		}
-		b, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
-		if err != nil {
-			colorlog.Error("read %s failed: %v", url, err)
-			os.Exit(1)
-		}
-		colorlog.Info("%s: %s", url, b)
+var urls = []string{"http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com", "http://www.baidu.com"}
+
+func fetch(url string, ch chan string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		colorlog.Error("fetch %s failed: %v", url, err)
+		os.Exit(1)
 	}
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		colorlog.Error("read %s failed: %v", url, err)
+		os.Exit(1)
+	}
+	if ch != nil {
+		ch <- string(b)
+	} else {
+		colorlog.Info("%s: %d", url, len(string(b)))
+	}
+}
+
+func TestFetch(t *testing.T) {
+	start := time.Now()
+	for _, url := range urls {
+		fetch(url, nil)
+	}
+	end := time.Now() // 2.85s
+	colorlog.Info("fetch all url cost %v", end.Sub(start))
+}
+
+func TestFetchAll(t *testing.T) {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range urls {
+		go fetch(url, ch)
+	}
+	for _, url := range urls {
+		s := <-ch
+		colorlog.Info("%s: %d", url, len(s))
+	}
+	end := time.Now() // 1.76s
+	colorlog.Info("fetch all url cost %v", end.Sub(start))
 }
