d1549b219f34740d784c11fed02122fb12348c38
itherunder <liaozhou98@qq.com>
2022-04-06 22:42:58 +0800

    echo server_ and http.Request's content

--- server_/server_test.go
+++ server_/server_test.go
@@ -6,6 +6,8 @@ import (
 	"net/http"
 	"sync"
 	"testing"
+
+	"github.com/yezihack/colorlog"
 )
 
 var mu sync.Mutex
@@ -38,4 +40,25 @@ func counter(w http.ResponseWriter, r *http.Request) {
 	mu.Lock()
 	fmt.Fprintf(w, "Count %d\n", count)
 	mu.Unlock()
+}
+
+func TestEchoServer(t *testing.T) {
+	http.HandleFunc("/echo", echo)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	colorlog.Info("%s %s %s", r.Method, r.URL, r.Proto)
+	for k, v := range r.Header {
+		colorlog.Info("Header[%q] = %q", k, v)
+	}
+	colorlog.Info("Host = %q", r.Host)
+	colorlog.Info("RemoteAddr = %q", r.RemoteAddr)
+	if err := r.ParseForm(); err != nil {
+		colorlog.Error("ParseForm() err: %v", err)
+	}
+	for k, v := range r.Form {
+		colorlog.Info("Form[%q] = %q", k, v)
+	}
+	w.Write([]byte("<h1>" + r.Form.Get("msg") + "</h1>"))
 }
