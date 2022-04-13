45559f24cd10947b8930f74bd4eb56b67ab61dbd
itherunder <liaozhou98@qq.com>
2022-04-13 23:05:00 +0800

    bytes buffer comma

--- bytes_/bytes_test.go
+++ bytes_/bytes_test.go
@@ -14,6 +14,7 @@ func TestBytes(t *testing.T) {
 	buf.Write([]byte{'l', 'o', 'n', 'g'})
 	fmt.Printf("buf: %v\n", buf.String())
 	fmt.Printf("ints: %v\n", intsToString([]int{1, 2, 3}))
+	fmt.Printf("comma: %v\n", comma(5233451))
 }
 
 func intsToString(vals []int) string {
@@ -25,4 +26,21 @@ func intsToString(vals []int) string {
 		buf.WriteString(fmt.Sprintf("%d", v))
 	}
 	return buf.String()
+}
+
+func comma(i int) string {
+	s := fmt.Sprintf("%d", i)
+	var buf bytes.Buffer
+	for i, c := len(s)-1, 0; i >= 0; i, c = i-1, c+1 {
+		if c > 0 && c%3 == 0 {
+			buf.WriteByte(',')
+		}
+		buf.WriteByte(s[i])
+	}
+	res := buf.Bytes()
+	l := len(res)
+	for i := 0; i < l/2; i++ {
+		res[i], res[l-i-1] = res[l-i-1], res[i]
+	}
+	return string(res)
 }
