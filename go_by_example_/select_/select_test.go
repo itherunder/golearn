e57952f9f171db04c59637bf0daa7b2728b64c69
itherunder <61953384+itherunder@users.noreply.github.com>
2022-04-27 10:42:28 +0800

    update select_

---
+++ go_by_example_/select_/select_test.go
@@ -0,0 +1,32 @@
+package select_
+
+import (
+	"testing"
+	"time"
+
+	"github.com/yezihack/colorlog"
+)
+
+func main() {
+	ch1 := make(chan string, 1)
+	ch2 := make(chan string, 1)
+	
+	go func() {
+		time.Sleep(1 * time.Second)
+		ch1 <- "one"
+	}()
+	
+	go func() {
+		time.Sleep(2 * time.Second)
+		ch2 <- "two"
+	}()
+	
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <- ch1:
+			colorlog.Info("received %v", msg1)
+		case msg2 := <- ch2:
+			colorlog.Info("received %v", msg2)
+		}
+	}
+}
