7b7812cb146053ef70a16168a9c8fda994645655
itherunder <liaozhou98@qq.com>
2022-05-15 16:09:48 +0800

    update go_by_example random numbers

---
+++ go_by_example_/random_numbers_/random_numbers_test.go
@@ -0,0 +1,37 @@
+package random_numbers_
+
+import (
+	"fmt"
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func TestRandomNumbers(t *testing.T) {
+	fmt.Print(rand.Intn(100), ",")
+	fmt.Print(rand.Intn(100))
+	fmt.Println()
+
+	fmt.Println(rand.Float64())
+
+	fmt.Print((rand.Float64()*5)+5, ",")
+	fmt.Print((rand.Float64() * 5) + 5)
+	fmt.Println()
+
+	s1 := rand.NewSource(time.Now().UnixNano())
+	r1 := rand.New(s1)
+
+	fmt.Print(r1.Intn(100), ",")
+	fmt.Print(r1.Intn(100))
+	fmt.Println()
+
+	s2 := rand.NewSource(42) // same source will generate smae random number series
+	r2 := rand.New(s2)
+	fmt.Print(r2.Intn(100), ",")
+	fmt.Print(r2.Intn(100))
+	fmt.Println()
+	s3 := rand.NewSource(42)
+	r3 := rand.New(s3)
+	fmt.Print(r3.Intn(100), ",")
+	fmt.Print(r3.Intn(100))
+}
