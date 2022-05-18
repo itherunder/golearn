85b67311ca829a7322710bb8c84ac58d68368da6
itherunder <liaozhou98@qq.com>
2022-05-18 22:05:06 +0800

    update go_by_example spawning processes

---
+++ go_by_example_/spawning_processes_/spawning_processes_test.go
@@ -0,0 +1,40 @@
+package spawning_processes_
+
+import (
+	"fmt"
+	"io"
+	"os/exec"
+	"testing"
+)
+
+func TestSpawningProcesses(t *testing.T) {
+	dateCmd := exec.Command("date")
+
+	dateOut, err := dateCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("> date")
+	fmt.Println(string(dateOut))
+
+	grepCmd := exec.Command("grep", "hello")
+	grepIn, _ := grepCmd.StdinPipe()
+	grepOut, _ := grepCmd.StdoutPipe()
+	grepCmd.Start()
+	grepIn.Write([]byte("hello grep\ngoodbye grep"))
+	grepIn.Close()
+	grepBytes, _ := io.ReadAll(grepOut)
+	grepCmd.Wait()
+
+	fmt.Println("> grep hello")
+	fmt.Println(string(grepBytes))
+
+	lsCmd := exec.Command("ls")
+	lsOut, err := lsCmd.Output()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("> ls")
+	fmt.Println(string(lsOut))
+}
