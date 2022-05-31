b1c004ecb7f42dc848b8d1cb7e9e939caba4e0ef
itherunder <liaozhou98@qq.com>
2022-05-31 13:40:14 +0800

    update advance go ... golang json rpc test support http use postman to request success

--- advanced_go_programming_book_/ch4_1/http_json_rpc_hello_world_test.go
+++ advanced_go_programming_book_/ch4_1/http_json_rpc_hello_world_test.go
@@ -1,1 +1,30 @@
 package ch4_1
+
+import (
+	"io"
+	"net/http"
+	"net/rpc"
+	"net/rpc/jsonrpc"
+	"testing"
+
+	"github.com/yezihack/colorlog"
+)
+
+func TestHttpJsonRpcHelloWorld(t *testing.T) {
+	err := RegistrerHelloService(new(HelloService))
+	if err != nil {
+		colorlog.Error("Register hello service error: %v", err)
+	}
+	http.HandleFunc("/jsonrpc", func(w http.ResponseWriter, r *http.Request) {
+		var conn io.ReadWriteCloser = struct {
+			io.Writer
+			io.ReadCloser
+		}{
+			w,
+			r.Body,
+		}
+		rpc.ServeCodec(jsonrpc.NewServerCodec(conn))
+	})
+
+	http.ListenAndServe(":1234", nil)
+}
